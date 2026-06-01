1a2ffaafa2f2aa1b33e3d63a924f6f335a63e691
4shey <tholiulasbah18@gmail.com>
2026-06-01 15:24:35 +0700

    add railway migration

--- app/main.go
+++ app/main.go
@@ -9,20 +9,44 @@ import (
 	database "one_more_percent/internal/db"
 	"one_more_percent/internal/routes"
 	"one_more_percent/internal/services"
+
+	"github.com/golang-migrate/migrate/v4"
+	_ "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
 )
 
+func runMigration() {
+	databaseURL := os.Getenv("DATABASE_URL")
+
+	m, err := migrate.New(
+		"file://database/migrations",
+		databaseURL,
+	)
+
+	if err != nil {
+		log.Fatal("migration init:", err)
+	}
+
+	err = m.Up()
+
+	if err != nil && err != migrate.ErrNoChange {
+		log.Fatal("migration up:", err)
+	}
+
+	fmt.Println("Migration completed ✓")
+}
+
 func main() {
-	// Connect to PostgreSQL.
 	if err := database.Connect(); err != nil {
 		log.Fatal("DB connection failed:", err)
 	}
 
-	// Start background scheduler (reminders + midnight recap).
+	runMigration()
+
 	services.StartScheduler()
 
 	router := routes.SetupRoutes()
 
-	// Railway will inject PORT automatically
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module one_more_percent
 go 1.26.3
 
 require (
-	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
-	github.com/lib/pq v1.12.3 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
+	github.com/golang-migrate/migrate/v4 v4.19.1
+	github.com/lib/pq v1.12.3
 )
